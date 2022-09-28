3f40b4687e3c53472ba0767f43d97bda264bedd9
junwuLin815 <114549068+junwuLin815@users.noreply.github.com>
2022-09-28 16:45:33 +0800

    Update GitTest.java

--- src/main/java/com/junwuLin/git/GitTest.java
+++ src/main/java/com/junwuLin/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是gie");
         System.out.println("hello4");
-        System.out.println("hot-fix");
+        System.out.println("hot-fixgai");
 
     }
 }
